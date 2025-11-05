cd2b0f1cccddc888698eba79950bfe3e688ce506
malthe-kea <mati0002@stud.kea.dk>
2025-11-05 13:00:46 +0100

    init commit

---
+++ src/main/java/dtos/MyResponse.java
@@ -0,0 +1,23 @@
+package dtos;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+import java.util.List;
+import java.util.Map;
+
+
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class MyResponse {
+    String answer;
+    List<Map<String, String>> messages;
+
+    public MyResponse(){}
+    public MyResponse(String answer) {
+        this.answer = answer;
+    }
+    public MyResponse(String answer, List<Map<String,String>> messages) {
+        this.answer = answer;
+        this.messages = messages;
+    }
+}
