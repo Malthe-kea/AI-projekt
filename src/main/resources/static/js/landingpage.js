c6b56682ec28dfe0a9240316bbe926f5b7bea39c
malthe-kea <mati0002@stud.kea.dk>
2025-11-05 15:14:32 +0100

    frontend er forsat

--- src/main/resources/static/js/landingpage.js
+++ src/main/resources/static/js/landingpage.js
@@ -7,16 +7,18 @@ function createPage() {
 
     const label = document.createElement("label");
     label.textContent = "Topic: ";
-    label.setAttribute("for", "the-question");
+    label.setAttribute("for", "the-topic");
 
     const topicInput = document.createElement("input");
-    topicInput.id = "the-question";
+    topicInput.id = "the-topic";
     topicInput.type = "text";
     topicInput.placeholder = "Skriv et emne...";
 
     const button = document.createElement("button");
     button.textContent = "Hent falsk fakta";
     button.addEventListener("click", getFalseFact);
+
+
 
     container.appendChild(label);
     container.appendChild(topicInput);
@@ -27,5 +29,15 @@ function createPage() {
 async function getFalseFact(event){
     event.preventDefault()
 
-    const url = `${SERVER_URL}fact?about=${document.getElementById('the-question').value}`
+    const inputValue = document.getElementById('the-topic').value;
+    const url = `${SERVER_URL}fact?about=${encodeURIComponent(inputValue)}`;
+
+
+    const resultDiv = document.createElement("p");
+    resultDiv.textContent = `Her ville jeg hente falsk fakta om: ${inputValue}`;
+    app.appendChild(resultDiv)
+
+    const response = await fetch(url);
+    const data = await response.json();
+    console.log(data)
 }
